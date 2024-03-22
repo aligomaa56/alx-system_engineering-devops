b6084a85c6d0395f73b674bcbe9ab72720fca7a2
aligomaa56 <3looosh56565656@gmail.com>
2024-03-22 20:31:58 +0200

    task-1

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+# Puppet manifest to create a file in /tmp
+file { '/tmp/school':
+  ensure  => file,
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}
