68d20927d232ec9cff2defae7e9d37cdad588cc1
aligomaa56 <3looosh56565656@gmail.com>
2024-04-24 20:11:56 +0200

    task-0

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+import requests
+import sys
+
+if __name__ == '__main__':
+    url = "https://jsonplaceholder.typicode.com/"
+    employee_id = sys.argv[1]
+
+    user_response = requests.get(f"{url}/users/{employee_id }")
+    todos_response = requests.get(f"{url}/todos?userId={employee_id}")
+
+    user_data = user_response.json()
+    todos_data = todos_response.json()
+
+    name = user_data.get('name')
+    completed_tasks = []
+    for task in todos_data:
+        if task.get('completed'):
+            completed_tasks.append(task.get('title'))
+    total_tasks = len(todos_data)
+
+    print(f"Employee {name} is done with\
+         tasks({len(completed_tasks)}/{total_tasks}):")
+    for task in completed_tasks:
+        print(f"\t {task}")
